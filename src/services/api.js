6481ec09f76ca9e9896810e1b826ba2263feae9f
CHIDURALADEEKSHITH <2400030364@kluniversity.in>
2026-05-26 12:17:19 +0530

    updated api url's

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:1995";
+const API_BASE_URL = "https://bhagyalaxmifertilizers.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
